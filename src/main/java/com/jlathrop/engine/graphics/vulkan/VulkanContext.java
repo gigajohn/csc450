ddd31cf3bc0182ca864851310707397071326530
John Lathrop <lathrop1009@gmail.com>
2026-07-06 15:57:40 -0500

    Refactor VulkanContext: added surface creation and refactored queue family selection logic

--- src/main/java/com/jlathrop/engine/graphics/vulkan/VulkanContext.java
+++ src/main/java/com/jlathrop/engine/graphics/vulkan/VulkanContext.java
@@ -1,30 +1,32 @@
 package com.jlathrop.engine.graphics.vulkan;
 
-import java.nio.LongBuffer;
-import static org.lwjgl.glfw.GLFWVulkan.glfwCreateWindowSurface;
-import static org.lwjgl.vulkan.KHRSurface.vkDestroySurfaceKHR;
-
 import org.lwjgl.PointerBuffer;
 import org.lwjgl.system.MemoryStack;
 import org.lwjgl.vulkan.*;
 
-import java.nio.IntBuffer; // Required for GPU counting
+import java.nio.IntBuffer;
+import java.nio.LongBuffer;
+import java.util.HashSet;
+import java.util.Set;
 
-import javax.management.RuntimeErrorException;
-
+import static org.lwjgl.glfw.GLFWVulkan.glfwCreateWindowSurface;
 import static org.lwjgl.glfw.GLFWVulkan.glfwGetRequiredInstanceExtensions;
 import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.vulkan.EXTDebugUtils.VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
+import static org.lwjgl.vulkan.KHRSurface.*;
 import static org.lwjgl.vulkan.VK10.*;
 
 public class VulkanContext {
     private VkInstance instance;
     private long surface;
     private VkPhysicalDevice physicalDevice;
-
     private VkDevice device;
-    private VkQueue graphicQueue;
+    
+    private VkQueue graphicsQueue;
     private int graphicsQueueFamilyIndex = -1;
+    
+    private VkQueue presentQueue;
+    private int presentQueueFamilyIndex = -1;
 
     private static final boolean ENABLE_VALIDATION_LAYERS = true;
     private static final String VALIDATION_LAYER = "VK_LAYER_KHRONOS_validation";
@@ -80,6 +82,17 @@ public class VulkanContext {
         }
     }
 
+    private void createSurface(long windowHandle) {
+        try (MemoryStack stack = stackPush()) {
+            LongBuffer pSurface = stack.mallocLong(1);
+            if (glfwCreateWindowSurface(instance, windowHandle, null, pSurface) != VK_SUCCESS) {
+                throw new RuntimeException("Failed to create Window Surface!");
+            }
+            surface = pSurface.get(0);
+            System.out.println("Window Surface successfully created!");
+        }
+    }
+
     private void pickPhysicalDevice() {
         try (MemoryStack stack = stackPush()) {
             IntBuffer deviceCount = stack.mallocInt(1);
@@ -94,7 +107,6 @@ public class VulkanContext {
 
             for (int i = 0; i < devices.capacity(); i++) {
                 VkPhysicalDevice device = new VkPhysicalDevice(devices.get(i), instance);
-                
                 if (isDeviceSuitable(device)) {
                     physicalDevice = device;
                     break;
@@ -117,7 +129,6 @@ public class VulkanContext {
 
             System.out.println("Checking GPU: " + deviceName);
 
-            // We want the dedicated or integrated hardware GPU
             boolean isHardwareGpu = deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU || 
                                     deviceType == VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU;
 
@@ -129,45 +140,62 @@ public class VulkanContext {
         }
     }
 
-    private void findQuueueFamilies(VkPhysicalDevice device){
-        try(MemoryStack stack = stackPush()){
+    private void findQueueFamilies(VkPhysicalDevice device) {
+        try (MemoryStack stack = stackPush()) {
             IntBuffer queueFamilyCount = stack.mallocInt(1);
             vkGetPhysicalDeviceQueueFamilyProperties(device, queueFamilyCount, null);
 
             VkQueueFamilyProperties.Buffer queueFamilies = VkQueueFamilyProperties.calloc(queueFamilyCount.get(0), stack);
             vkGetPhysicalDeviceQueueFamilyProperties(device, queueFamilyCount, queueFamilies);
 
-            for( int i = 0; i < queueFamilies.capacity(); i++){
+            IntBuffer presentSupport = stack.mallocInt(1);
+
+            for (int i = 0; i < queueFamilies.capacity(); i++) {
                 if ((queueFamilies.get(i).queueFlags() & VK_QUEUE_GRAPHICS_BIT) != 0) {
                     graphicsQueueFamilyIndex = i;
+                }
+
+                vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, presentSupport);
+                if (presentSupport.get(0) == VK_TRUE) {
+                    presentQueueFamilyIndex = i;
+                }
+
+                if (graphicsQueueFamilyIndex != -1 && presentQueueFamilyIndex != -1) {
                     break;
                 }
             }
-            if(graphicsQueueFamilyIndex == -1) throw new RuntimeException("Failed to find a suitable graphics queue family");
 
+            if (graphicsQueueFamilyIndex == -1 || presentQueueFamilyIndex == -1) {
+                throw new RuntimeException("Failed to find suitable queue families for graphics and presentation!");
+            }
         }
     }
 
-    private void createLogicalDevice(){
-        findQuueueFamilies(physicalDevice);
+    private void createLogicalDevice() {
+        findQueueFamilies(physicalDevice);
 
-        try(MemoryStack stack = stackPush()){
-            VkDeviceQueueCreateInfo.Buffer queueCreateInfos = VkDeviceQueueCreateInfo.calloc(1,stack);
-            queueCreateInfos.get(0).sType(VK_STRUCTURE_TYPE_EVENT_CREATE_INFO);
-            queueCreateInfos.get(0).queueFamilyIndex(graphicsQueueFamilyIndex);
+        try (MemoryStack stack = stackPush()) {
+            Set<Integer> uniqueQueueFamilies = new HashSet<>();
+            uniqueQueueFamilies.add(graphicsQueueFamilyIndex);
+            uniqueQueueFamilies.add(presentQueueFamilyIndex);
 
-            //priority
-            queueCreateInfos.get(0).pQueuePriorities(stack.floats(1.0f));
+            VkDeviceQueueCreateInfo.Buffer queueCreateInfos = VkDeviceQueueCreateInfo.calloc(uniqueQueueFamilies.size(), stack);
 
-            //features needed
+            int i = 0;
+            for (Integer queueFamily : uniqueQueueFamilies) {
+                VkDeviceQueueCreateInfo queueCreateInfo = queueCreateInfos.get(i++);
+                queueCreateInfo.sType(VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO);
+                queueCreateInfo.queueFamilyIndex(queueFamily);
+                queueCreateInfo.pQueuePriorities(stack.floats(1.0f));
+            }
+
             VkPhysicalDeviceFeatures deviceFeatures = VkPhysicalDeviceFeatures.calloc(stack);
 
-            //create logial dev
             VkDeviceCreateInfo createInfo = VkDeviceCreateInfo.calloc(stack);
             createInfo.sType(VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO);
             createInfo.pQueueCreateInfos(queueCreateInfos);
             createInfo.pEnabledFeatures(deviceFeatures);
-            
+
             PointerBuffer pDevice = stack.mallocPointer(1);
             if (vkCreateDevice(physicalDevice, createInfo, null, pDevice) != VK_SUCCESS) {
                 throw new RuntimeException("Failed to create logical device");
@@ -177,30 +205,24 @@ public class VulkanContext {
 
             PointerBuffer pQueue = stack.mallocPointer(1);
             vkGetDeviceQueue(device, graphicsQueueFamilyIndex, 0, pQueue);
-            graphicQueue = new VkQueue(pQueue.get(0), device);
+            graphicsQueue = new VkQueue(pQueue.get(0), device);
 
-            System.out.println("Logical Device and Graphical Queue succesfully created");
-        }
-
-    }
-
-    private void createSurface(long windowHandle){
-        try(MemoryStack stack = stackPush()){
-            LongBuffer pSurface = stack.mallocLong(1);
-            if (glfwCreateWindowSurface(instance, windowHandle, null, pSurface) != VK_SUCCESS) throw new RuntimeException("Failed to create Window Surface");
-
-            surface =pSurface.get(0);
-            System.out.println("Window Surface successfully created");
+            vkGetDeviceQueue(device, presentQueueFamilyIndex, 0, pQueue);
+            presentQueue = new VkQueue(pQueue.get(0), device);
+            
+            System.out.println("Logical Device, Graphics Queue, and Present Queue successfully created!");
         }
     }
 
     public void cleanup() {
-        if(device != null)
+        if (device != null) {
             vkDestroyDevice(device, null);
-        if (surface !=0)
+        }
+        if (surface != 0) {
             vkDestroySurfaceKHR(instance, surface, null);
-        if (instance != null) 
+        }
+        if (instance != null) {
             vkDestroyInstance(instance, null);
-        
+        }
     }
 }
